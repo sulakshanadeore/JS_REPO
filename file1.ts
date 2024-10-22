6c3397060daea82e4058c8511be0bdcfdf29526e
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2024-10-22 15:25:54 +0530

    Add files via upload

---
+++ file1.ts
@@ -0,0 +1,23 @@
+ let i=0,j=0,ans=0;
+ function add(a,b)
+{
+ans=a+b;
+return ans;
+}
+
+
+ function multiply(a,b)
+{
+ans=a*b;
+return ans;
+}
+
+//  i=10;
+// j=100;
+// let myans=add();
+// console.log(myans);
+
+// i=10;
+// j=20;
+// let mymultians=multiply();
+// console.log(mymultians);
---
+++ file2.ts
@@ -0,0 +1,24 @@
+
+let a=0,b=0;
+ function subtract(a, b) {
+    let myans = a - b;
+    return myans;
+}
+ function divide(a, b) {
+    let myans = a / b;
+    return myans;
+}
+
+// newAdd(10,20);
+// console.log(ans);
+//add(10,20);
+//console.log()
+// function callSum()
+// {
+//     return add(a,b);
+// }
+
+// function callmultiply()
+// {
+
+//     return multiply(a,b);
